d2dce93b7ae990eae8162e09fa947048ee26825b
Senad Zukorlic <zukorlic.senad01@gmail.com>
2024-10-14 17:06:37 +0200

    first commit

---
+++ main.c
@@ -0,0 +1,37 @@
+#include <stdio.h> //osnovna biblioteka
+#include <math.h>
+// double // decimalni
+// long // decimalni
+// float // 
+// char // definisanje karaktera 
+// %d oznacava integer
+// \n- new line je da ubaci u novi red
+// int main() //glavni deo u kojem se svi procesi izvrsavaju
+// {
+//     // int a; // int - integer,koristi se za celobrojne vrednosti
+//     // int b;
+//     double a;
+//      double rez-sqrt;
+//       double rez-log ; 
+//       double rez-log10 ;
+//     rez-sqrt = sqrt(a)
+//     rez-log= log(a)
+//     // printf("Unesi broj a: "); // stampanje
+//     scanf("%d", &a); // skener,
+//      printf("Unesi broj b: ");
+//     scanf("%d", &b); // skener,
+//     rezultat = a+ b;
+//     printf("Zbir broja a i b je: %d\n", rezultat);
+//     return 0;
+// }
+
+//  double a;
+//     double rez_sqrt;
+//     double rez_log ; 
+//     double rez_log10;
+//     rez_sqrt = sqrt(a);
+//     rez_log = log(a);
+//     rez_log10=log10(a);
+//     printf("sqrt(%.2lf) = %.2lf\nlog(%.2lf) = %.2lf\nlog10(%.2lf) = %.2lf\n", a, rez_sqrt, a, rez_log, a, rez_log10);
+   
+//     return 0;
