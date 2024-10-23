71437a86f2aec85d62c2566eb23d747ceca4646f
Senad Zukorlic <zukorlic.senad01@gmail.com>
2024-10-23 22:16:33 +0200

    worked home work

---
+++ lessons/class2.c
@@ -0,0 +1,20 @@
+
+#include <stdio.h>
+
+int main (){
+    int a;
+  int noviBroj;
+
+    printf("Unesite broj: ");
+    scanf("%d",&a);
+
+    if(a < 0 ){
+       noviBroj = a+1;
+
+    }else{
+       noviBroj= a-1;
+    }
+  printf("%d\n",noviBroj);
+    
+    return 0;
+}
